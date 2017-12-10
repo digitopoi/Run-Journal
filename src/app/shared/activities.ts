952c06cff79ca011b2185d1a1ed61ebc89c5a6b8
digitopoi <hubo85@gmail.com>
2017-12-10 12:21:08 -0600

    Add activity data

---
+++ src/app/shared/activities.ts
@@ -0,0 +1,12 @@
+import { IActivity } from './activity.model';
+
+export const SAVED_ACTIVITIES: IActivity[] = [
+    {
+        'id': 1,
+        'name': 'Main Bike Trails',
+        'date': new Date('06/01/2017'),
+        'distance': 26.2,
+        'comments': 'Nice day, cool temps',
+        'gpxData': '../../assets/gpx/route2057479.gpx'
+    }
+];
