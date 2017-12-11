a7265c0b4d6b4ad0d5590a6d31674579942f9516
digitopoi <hubo85@gmail.com>
2017-12-10 19:09:04 -0600

    Add map component and map service

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { MapService } from '../services/map.service';
+import { IActivity } from '../shared/activity.model';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-map',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapComponent implements OnInit {
 
-  constructor() { }
+  activity: any;
+  activityName: string;
+  activityComments: string;
+  activityDistance: number;
+  gpx: any;
+
+  constructor(
+    private _mapService: MapService,
+    private _route: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.activity = this._mapService.getActivity(
+      +this._route.snapshot.params['id']);
   }
 
 }
---
+++ src/app/services/map.service.ts
@@ -0,0 +1,12 @@
+import { SAVED_ACTIVITIES } from './../shared/activities';
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { IActivity } from '../shared/activity.model';
+import { ActivityService } from '../services/activity.service';
+
+@Injectable()
+export class MapService {
+
+  constructor() { }
+
+}
